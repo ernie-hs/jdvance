595ddfafda4d659d7129570377b602357c3f9731
ernie <ernie_hs@hotmail.com>
2025-06-03 14:34:57 +0200

    stuff

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # ad vanced test framework
 
 pweese apologise for this
+
+```sh
+jdvance file.js
+```
+
+```js 
+import { it, expect } from "jdvance";
+
+it("shood say pleeze", () => {
+    let pweeze = false;
+
+    expect(pweeze).to.be.equal(true);
+});
+```
+da
--- test.js
+++ test.js
@@ -1,11 +1,22 @@
-import expect from "./lib/index.js";
+import { it, expect } from "./lib/index.js";
 
-expect(1).to.be.equal(1);
-expect(1).to.not.be.equal(2);
-expect(1).to.not.not.be.equal(1);
-expect(1).to.not.not.not.be.equal(2);
-expect(2).to.greaterThan(1);
-expect(1).to.not.greaterThan(3);
-expect(1).lessThan(2);
-expect(1).to.lessThanOrEqual(1);
-expect(1).to.greaterThanOrEqual(1);
+it("is empty");
+
+it("expect...", () => {
+    expect(1).to.be.equal(1);
+    expect(1).to.not.be.equal(2);
+    expect(1).notEqual(2);
+    expect(1).to.not.not.be.equal(1);
+    expect(1).to.not.not.not.be.equal(2);
+    expect(2).to.greaterThan(1);
+    expect(1).to.not.greaterThan(3);
+    expect(1).lessThan(2);
+    expect(1).to.lessThanOrEqual(1);
+    expect(1).to.greaterThanOrEqual(1);
+
+    it("haz hamburger", function() {
+        let pweeze = false;
+
+        expect(pweeze).to.not.be.equal(true, "pweeze is false");
+    });
+});
